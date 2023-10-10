854f36b77ac6bef068f677a3784e80bb9545fa27
alexander.kudlay <alexander.kudlay@x2sy.com>
2023-10-10 20:38:34 +0300

    add new system line

--- Src/KukaLesson04Git.java
+++ Src/KukaLesson04Git.java
@@ -3,6 +3,7 @@ public class KukaLesson04Git {
     public static void main(String[] args) {
 
         System.out.println("hello world 4 GIt Вимучили");
+        System.out.println("second comit to git");
     }
 
 }
